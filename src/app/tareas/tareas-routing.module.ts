6b4d64cfdd9a9c8fdd7f2f35917254f3a7f848ce
freaktiful <freaktifulwhispers@gmail.com>
2019-11-26 13:39:23 +0100

    subenrutamiento estructura

--- src/app/tareas/tareas-routing.module.ts
+++ src/app/tareas/tareas-routing.module.ts
@@ -1,10 +1,22 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { MainComponent } from './main/main.component';
+import { TareaSimpleComponent } from './tarea-simple/tarea-simple.component';
+import { TareaPlusComponent } from './tarea-plus/tarea-plus.component';
 
 
 const routes: Routes = [
   { path: '', component: MainComponent },
+  /* aquí tengo que definir los subenrutamientos a-route-path-with-children */
+  {
+    path: '', 
+    component: MainComponent,
+    children: [
+      { path: 'simple', component: TareaSimpleComponent },
+      { path: 'plus', component: TareaPlusComponent }
+    ]
+  }
+  
 ];
 
 @NgModule({
--- src/app/tareas/tareas.module.ts
+++ src/app/tareas/tareas.module.ts
@@ -3,10 +3,13 @@ import { CommonModule } from '@angular/common';
 
 import { TareasRoutingModule } from './tareas-routing.module';
 import { MainComponent } from './main/main.component';
+import { MenuComponent } from './menu/menu.component';
+import { TareaSimpleComponent } from './tarea-simple/tarea-simple.component';
+import { TareaPlusComponent } from './tarea-plus/tarea-plus.component';
 
 
 @NgModule({
-  declarations: [MainComponent],
+  declarations: [MainComponent, MenuComponent, TareaSimpleComponent, TareaPlusComponent],
   imports: [
     CommonModule,
     TareasRoutingModule
